c5917c06ec41beed63be1a5a9f6ab86015d65a30
Ajwood <awoody@protonmail.com>
2019-06-29 13:21:59 +0930

    creating functions. they are not very intuitive at all. not start or end. and spaces mean much more than just file formatting.

--- Testing.py
+++ Testing.py
@@ -4,16 +4,57 @@
 
 print("Take a bow")
 
-i = 1
 
-while i < 3:
-  print(i)
-  i += 1
 
-#Enable retrieving user input
-print("please provide a widget")
-response = input()
-print ("the widget you provided was: ")
-print(response)
+def tri_recursion(k):
+  
+  if(k>0):
+    result = k+tri_recursion(k-1)
+    print(result)
+  else:
+    result = 0
+  return result
 
+print("\n\nRecursion Example Results")
+tri_recursion(3)
+
+def exampleForLoop():
+  fruits = ["apple", "banana", "cherry"]
+  for x in fruits:
+    print(x)
+
+print("\n\nFor loop example")
+exampleForLoop()
+
+def theEncodings():
+  #convert between bytes and string
+
+  norsk = "the quick brown dog"
+  print(norsk)
+  data = norsk.encode('utf-8')
+  print (data)
+
+theEncodings()
+
+def askQuestions():
+      
+  print("please provide a widget")
+  response = input()
+  print ("the widget you provided was: ")
+  print(response)
+
+
+def exampleWhileLoop(i):
+  while (i < 3):
+    print(i)
+    i += 1
+
+exampleWhileLoop(4)
+
+#List vs dictionaries
+
+def exampleList():
+  list("bob")
+
+exampleList()
 
