7b541ab008a0591df078c960b500445186445712
Ajwood <awoody@protonmail.com>
2019-06-29 11:51:57 +0930

    Enable user input. and a simple for loop.

--- Testing.py
+++ Testing.py
@@ -1,6 +1,19 @@
 #/usr/bin/python3
 
-import this
+#import this
 
 print("Take a bow")
+
+i = 1
+
+while i < 3:
+  print(i)
+  i += 1
+
+#Enable retrieving user input
+print("please provide a widget")
+response = input()
+print ("the widget you provided was: ")
+print(response)
+
 
